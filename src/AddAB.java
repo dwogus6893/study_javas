ab21bf55944ea2cb9efe8e895cb1d146f0322699
dwogus6893 <dwogus6893@naver.com>
2022-10-12 13:40:12 +0900

    2022-10-12

---
+++ src/AddAB.java
@@ -0,0 +1,12 @@
+// refer : https://www.acmicpc.net/submit/1000/50344558
+
+import java.util.Scanner;
+public class AddAB {
+     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int A = s.nextInt(); 
+        int B = s.nextInt();
+        System.out.println(A+B);
+        s.close();
+    }
+}
